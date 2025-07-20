3f774516203fa486dc863acb6049314b6126acf2
LuisFerRodVar <luisfrv1@estudiantec.cr>
2025-07-20 13:08:17 -0600

    Added functionality of get current workspace

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -2,9 +2,28 @@ import app from "ags/gtk4/app"
 import { Astal, Gtk } from "ags/gtk4"
 import { execAsync } from "ags/process"
 import { createPoll } from "ags/time"
+import { With } from "ags"
 
 export default function Bar(monitor = 0) {
     const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+
+    const currentWorkspace = createPoll<number>(
+        1,
+        300,
+        async () => {
+            return execAsync("hyprctl activeworkspace -j")
+                .then(output => JSON.parse(output).id ?? 1)
+                .catch(() => 1);
+        }
+    );
+
+    const workspaceLabels = (ids: number[], current: number) =>
+        ids.map(id => (
+            <label
+                label={id.toString()}
+                class={`bar-title-text${id === current ? " current" : ""}`}
+            />
+        ));
 
     return (
         <window
@@ -22,38 +41,44 @@ export default function Bar(monitor = 0) {
                 hexpand
                 visible
                 class="bar">
-                <box
-                    hexpand
-                    class="bar-title"
-                    halign={Gtk.Align.CENTER}
-                    spacing={30}
-                >
-                    <label label="1" class="bar-title-text" />
-                    <label label="2" class="bar-title-text" />
-                    <label label="3" class="bar-title-text" />
-                    <label label="4" class="bar-title-text" />
-                    <label label="5" class="bar-title-text" />
-                </box>
-                <box
-                    halign={Gtk.Align.CENTER}
-                    hexpand
-                    class="bar-title"
-                >
-                    <label label="Astal" class="bar-title-text" />
-                </box>
-                <box
-                    halign={Gtk.Align.CENTER}
-                    spacing={30}
-                    hexpand
-                    class="bar-title"
-                >
-                    <label label="6" class="bar-title-text" />
-                    <label label="7" class="bar-title-text" />
-                    <label label="8" class="bar-title-text" />
-                    <label label="9" class="bar-title-text" />
-                    <label label="0" class="bar-title-text" />
-                </box>
+                <With value={currentWorkspace}>
+                    {current => (
+                        <box
+                            orientation={Gtk.Orientation.HORIZONTAL}
+                            halign={Gtk.Align.FILL}
+                            valign={Gtk.Align.CENTER}
+                            hexpand
+                            visible
+                            class="bar"
+                        >
+                            <box
+                                hexpand
+                                class="bar-title"
+                                halign={Gtk.Align.CENTER}
+                                spacing={30}
+                            >
+                                {workspaceLabels([1, 2, 3, 4, 5], current)}
+                            </box>
+                            <box
+                                halign={Gtk.Align.CENTER}
+                                hexpand
+                                class="bar-title"
+                            >
+                                <label label="Astal" class="bar-title-text" />
+                            </box>
+                            <box
+                                halign={Gtk.Align.CENTER}
+                                spacing={30}
+                                hexpand
+                                class="bar-title"
+                            >
+                                {workspaceLabels([6, 7, 8, 9, 0], current)}
+                            </box>
+                        </box>
+                    )}
+                </With>
             </box>
         </window >
     )
 }
+
